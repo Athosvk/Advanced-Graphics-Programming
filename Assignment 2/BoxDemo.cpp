856b7d116b597d4c3263d11b5e94df62991d6ca8
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-03-08 19:39:24 +0100

    Added basic mesh loading

---
+++ Assignment 2/BoxDemo.cpp
@@ -0,0 +1,229 @@
+//***************************************************************************************
+// BoxDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates rendering a colored box.
+//
+// Controls:
+//		Hold the left mouse button down and move the mouse to rotate.
+//      Hold the right mouse button down to zoom in and out.
+//
+//***************************************************************************************
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "MathHelper.h"
+#include "BoxDemo.h"
+
+const float BoxApp::KeyProcessInterval = 0.4f;
+
+BoxApp::BoxApp(HINSTANCE hInstance)
+: D3DApp(hInstance), mFX(0), mTech(0),
+  mfxWorldViewProj(0), mInputLayout(0), 
+  mTheta(1.5f*MathHelper::Pi), mPhi(0.25f*MathHelper::Pi), mRadius(5.0f)
+{
+	mMainWndCaption = L"Box Demo";
+	
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mWorld, I);
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+    mEnable4xMsaa = true;
+}
+
+BoxApp::~BoxApp()
+{
+	ReleaseCOM(mFX);
+	ReleaseCOM(mInputLayout);
+    ReleaseCOM(mWireframeState);
+    ReleaseCOM(mRegularState);
+}
+
+bool BoxApp::Init()
+{
+    if(!D3DApp::Init())
+    {
+        return false;
+    }
+
+    CreateRasterizerStates();
+	BuildFX();
+	BuildVertexLayout();
+    md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    m_Mesh = std::make_unique<Mesh>("Assets/Models/Sphere.obj", md3dDevice);
+	return true;
+}
+
+void BoxApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	// The window resized, so update the aspect ratio and recompute the projection matrix.
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void BoxApp::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	// Build the view matrix.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&mView, V);
+
+    if(GetAsyncKeyState('1') & 0x8000 && mKeyTimer >= KeyProcessInterval)
+    {
+        mKeyTimer -= KeyProcessInterval;
+        mCurrentState = mCurrentState == mWireframeState ? mRegularState : mWireframeState;
+        md3dImmediateContext->RSSetState(mCurrentState);
+    }
+    mKeyTimer += dt;
+}
+
+void BoxApp::DrawScene()
+{
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	md3dImmediateContext->IASetInputLayout(mInputLayout);
+    m_Mesh->bind(md3dImmediateContext);
+
+	// Set constants
+	XMMATRIX world = XMLoadFloat4x4(&mWorld);
+	XMMATRIX view  = XMLoadFloat4x4(&mView);
+	XMMATRIX proj  = XMLoadFloat4x4(&mProj);
+	XMMATRIX worldViewProj = world*view*proj;
+
+	mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+
+    D3DX11_TECHNIQUE_DESC techDesc;
+    mTech->GetDesc( &techDesc );
+    for(UINT p = 0; p < techDesc.Passes; ++p)
+    {
+        mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+        
+        m_Mesh->draw(md3dImmediateContext);
+    }
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void BoxApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void BoxApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void BoxApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if( (btnState & MK_LBUTTON) != 0 )
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi   += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi-0.1f);
+	}
+	else if( (btnState & MK_RBUTTON) != 0 )
+	{
+		// Make each pixel correspond to 0.005 unit in the scene.
+		float dx = 0.005f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.005f*static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void BoxApp::BuildFX()
+{
+	DWORD shaderFlags = 0;
+#if defined( DEBUG ) || defined( _DEBUG )
+    shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+ 
+	ID3D10Blob* compiledShader = 0;
+	ID3D10Blob* compilationMsgs = 0;
+	HRESULT hr = D3DX11CompileFromFile(L"FX/color.fx", 0, 0, 0, "fx_5_0", shaderFlags, 
+		0, 0, &compiledShader, &compilationMsgs, 0);
+
+	// compilationMsgs can store errors or warnings.
+	if( compilationMsgs != 0 )
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+		ReleaseCOM(compilationMsgs);
+	}
+
+	// Even if there are no compilationMsgs, check to make sure there were no other errors.
+	if(FAILED(hr))
+	{
+		DXTrace(__FILE__, (DWORD)__LINE__, hr, L"D3DX11CompileFromFile", true);
+	}
+
+	HR(D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(), 
+		0, md3dDevice, &mFX));
+
+	// Done with compiled shader.
+	ReleaseCOM(compiledShader);
+
+	mTech = mFX->GetTechniqueByName("ColorTech");
+	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void BoxApp::BuildVertexLayout()
+{
+	// Create the vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+
+	// Create the input layout
+    D3DX11_PASS_DESC passDesc;
+    mTech->GetPassByIndex(0)->GetDesc(&passDesc);
+	HR(md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature, 
+		passDesc.IAInputSignatureSize, &mInputLayout));
+}
+
+void BoxApp::CreateRasterizerStates()
+{
+    D3D11_RASTERIZER_DESC rasterizerDescription;
+    ZeroMemory(&rasterizerDescription, sizeof(D3D11_RASTERIZER_DESC));
+    rasterizerDescription.FillMode = D3D11_FILL_MODE::D3D11_FILL_WIREFRAME;
+    rasterizerDescription.CullMode = D3D11_CULL_MODE::D3D11_CULL_BACK;
+    rasterizerDescription.FrontCounterClockwise = false;
+    rasterizerDescription.DepthClipEnable = true;
+    HR(md3dDevice->CreateRasterizerState(&rasterizerDescription, &mWireframeState));
+
+    rasterizerDescription.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
+    HR(md3dDevice->CreateRasterizerState(&rasterizerDescription, &mRegularState));
+    mCurrentState = mRegularState;
+}
---
+++ Assignment 2/Mesh.cpp
@@ -0,0 +1,100 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <d3dUtil.h>
+
+#include "Mesh.h"
+
+Mesh::Mesh(const std::string& a_Filepath, ID3D11Device* a_Device)
+{
+    loadMeshdata(a_Filepath);
+    buildVertexBuffer(a_Device);
+    buildIndexBuffer(a_Device);
+}
+
+Mesh::~Mesh()
+{
+    ReleaseCOM(m_VertexBuffer);
+    ReleaseCOM(m_IndexBuffer);
+}
+
+void Mesh::draw(ID3D11DeviceContext* a_Context)
+{
+    a_Context->DrawIndexed(m_Indices.size(), 0, 0);
+}
+
+void Mesh::bind(ID3D11DeviceContext* a_Context)
+{
+    a_Context->IASetIndexBuffer(m_IndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+    auto vertexStride = sizeof(Vertex);
+    UINT offset = 0;
+    a_Context->IASetVertexBuffers(0, 1, &m_VertexBuffer, &vertexStride, &offset);
+}
+
+XMFLOAT2 Mesh::toXMFloat2(aiVector2D a_Vector)
+{
+    return XMFLOAT2(a_Vector.x, a_Vector.y);
+}
+
+XMFLOAT3 Mesh::toXMFloat3(aiVector3D a_Vector)
+{
+    return XMFLOAT3(a_Vector.x, a_Vector.y, a_Vector.z);
+}
+
+void Mesh::loadMeshdata(const std::string& a_Filepath)
+{
+    Assimp::Importer importer;
+    const auto scene = importer.ReadFile(a_Filepath, aiPostProcessSteps::aiProcess_CalcTangentSpace |
+        aiPostProcessSteps::aiProcess_Triangulate |
+        aiPostProcessSteps::aiProcess_JoinIdenticalVertices |
+        aiPostProcessSteps::aiProcess_SortByPType);
+    auto error = importer.GetErrorString();
+    auto mesh = scene->mMeshes[0];
+
+    m_Vertices.reserve(mesh->mNumVertices);
+    for(auto i = 0u; i < mesh->mNumVertices; i++)
+    {
+        m_Vertices.emplace_back(toXMFloat3(mesh->mVertices[i]));
+    }
+    m_Indices.reserve(mesh->mNumFaces * 3);
+    for(auto i = 0u; i < mesh->mNumFaces; ++i)
+    {
+        aiFace currentFace = mesh->mFaces[i];
+        for(auto j = 0; j < currentFace.mNumIndices; ++j)
+        {
+            m_Indices.push_back(currentFace.mIndices[j]);
+        }
+    }
+}
+
+void Mesh::buildVertexBuffer(ID3D11Device* a_Device)
+{
+    D3D11_BUFFER_DESC vertexBufferDescription
+    {
+        sizeof(Vertex) * m_Vertices.size(),
+        D3D11_USAGE_IMMUTABLE,
+        D3D11_BIND_VERTEX_BUFFER,
+        0,
+        0,
+        0,
+    };
+    D3D11_SUBRESOURCE_DATA vertexData;
+    vertexData.pSysMem = m_Vertices.data();
+    HR(a_Device->CreateBuffer(&vertexBufferDescription, &vertexData, &m_VertexBuffer));
+}
+
+void Mesh::buildIndexBuffer(ID3D11Device* a_Device)
+{
+    D3D11_BUFFER_DESC indexBufferDescription =
+    {
+        sizeof(UINT) * m_Indices.size(),
+        D3D11_USAGE_IMMUTABLE,
+        D3D11_BIND_INDEX_BUFFER,
+        0,
+        0,
+        0,
+    };
+    D3D11_SUBRESOURCE_DATA indexData;
+    indexData.pSysMem = m_Indices.data();
+    HR(a_Device->CreateBuffer(&indexBufferDescription, &indexData, &m_IndexBuffer));
+}
