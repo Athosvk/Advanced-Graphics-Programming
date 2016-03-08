ec0df327a7587edb449ff7f02bae54d9b6ac96e9
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-03-08 21:24:43 +0100

    Restored starting of the assignment

--- Assignment 1/Main.cpp
+++ Assignment 1/Main.cpp
@@ -7,20 +7,20 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
     PSTR cmdLine, int showCmd)
 {
-//    // Enable run-time memory check for debug builds.
-//#if defined(DEBUG) | defined(_DEBUG)
-//    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-//#endif
-//
-//    BoxApp theApp(hInstance);
-//
-//    if(!theApp.Init())
-//    {
-//        return EXIT_FAILURE;
-//    }
-//
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-    AGPEngine::Game game(600, 400, L"Aaaaa", hInstance);
-    game.run();
+    BoxApp theApp(hInstance);
+
+    if(!theApp.Init())
+    {
+        return EXIT_FAILURE;
+    }
+
+
+    //AGPEngine::Game game(600, 400, L"Aaaaa", hInstance);
+    //game.run();
     return 0;
 }
