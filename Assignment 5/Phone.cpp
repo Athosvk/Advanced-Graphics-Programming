31a2d50ca7d96603d70dabcf862e1133470ad230
Athos <athosvankralingen@hotmail.com>
2016-04-03 21:27:11 +0200

    Added quad for phone display

--- Assignment 5/Phone.cpp
+++ Assignment 5/Phone.cpp
@@ -13,6 +13,7 @@ void Phone::setShader(ID3DX11Effect* a_Shader)
 {
     m_Shader = a_Shader;
     m_Box.setShader(a_Shader);
+    m_Display.setShader(a_Shader);
 }
 
 void Phone::initialise(ID3D11Device* a_Device)
@@ -20,9 +21,12 @@ void Phone::initialise(ID3D11Device* a_Device)
     m_Box.initialise(a_Device);
     m_Box.constructVertexBuffer(a_Device);
     m_Box.constructIndexBuffer(a_Device);
+    m_Display.initialise(a_Device);
+    m_Display.initialiseBuffers(a_Device);
 }
 
 void Phone::draw(ID3D11DeviceContext* a_Context, CXMMATRIX a_ViewProjection)
 {
-    m_Box.draw(a_Context, a_ViewProjection);
+    //m_Box.draw(a_Context, a_ViewProjection);
+    m_Display.draw(a_Context, a_ViewProjection);
 }
--- Assignment 5/Phone.h
+++ Assignment 5/Phone.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Box.h"
+#include "Quad.h"
 
 class Phone
 {
 private:
     Box m_Box = Box(XMVectorSet(0.4f, 1.0f, 0.1f, 0.0f), L"Assets/Textures/Phone.png");
     ID3DX11Effect* m_Shader;
+    Quad m_Display = Quad(XMVectorSet(0.2f, 1.0f, 0.0f, 0.0f), L"Assets/Textures/Display.png");
 
 public:
     Phone();
