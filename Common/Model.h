8e3b88a95bc7283786a5d848133df2db8b1ae881
Athos <athosvankralingen@hotmail.com>
2016-05-13 11:38:02 +0200

    Updated documentation, updated some variable naming

--- Common/Model.h
+++ Common/Model.h
@@ -7,25 +7,94 @@
 
 #include "Vertex.h"
 
+/// <summary>
+/// Represents a mesh that can be loaded from disk
+/// </summary>
+/// <remarks> 
+/// Not to be used for meshes that contain submeshes, does not load materials
+/// Supports the same file formats as supported by the Open Asset Import Library (assimp)
+/// </remarks>
 class Model
 {
-private:
-    std::vector<Vertex> m_Vertices;
-    std::vector<UINT> m_Indices;
-    ID3D11Buffer* m_VertexBuffer;
-    ID3D11Buffer* m_IndexBuffer;
+private:	
+	/// <summary>
+	/// The mirrored CPU list of vertices to be used for the mesh
+	/// </summary>
+	std::vector<Vertex> m_Vertices;	
+	/// <summary>
+	/// The mirrored CPU list of the indices to be used for the mesh
+	/// </summary>
+	std::vector<UINT> m_Indices;	
+	/// <summary>
+	/// The handle to the vertex buffer resource, so we can bind it at run-time for rendering
+	/// </summary>
+	ID3D11Buffer* m_VertexBuffer;	
+	/// <summary>
+	/// The handle to the index buffer resource, so we can bind it at run-time for rendering
+	/// </summary>
+	ID3D11Buffer* m_IndexBuffer;
 
-public:
-    Model(const std::string& a_Filepath, ID3D11Device* a_Device);
-    ~Model();
+public:	
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Model"/> class.
+	/// </summary>
+	/// <param name="a_Filepath">The path to the model </param>
+	/// <param name="a_Device">The D3D11 device to allocate the buffers on </param>
+	Model(const std::string& a_Filepath, ID3D11Device* a_Device);
 
-    void draw(ID3D11DeviceContext* a_Contex);
-    void bind(ID3D11DeviceContext* a_Context);
-private:
-    XMFLOAT2 toXMFloat2(aiVector3D a_Vector);
-    XMFLOAT3 toXMFloat3(aiVector3D a_Vector);
-    XMFLOAT4 toXMFloat4(aiColor4D a_Color);
-    void loadMeshdata(const std::string& a_Filepath);
-    void buildIndexBuffer(ID3D11Device* a_Device);
-    void buildVertexBuffer(ID3D11Device* a_Device);
+	/// <summary>
+	/// Finalizes an instance of the <see cref="Model"/> class and de-allocates vertex and index buffers
+	/// </summary>
+	~Model();
+	
+	/// <summary>
+	/// Draws the model using the specified device context
+	/// </summary>
+	/// <param name="a_Contex">The device context</param>
+	void draw(ID3D11DeviceContext* a_Context);
+	
+	/// <summary>
+	/// Binds the buffers of this model to the specified context, so that the buffers can be bound to it
+	/// </summary>
+	/// <param name="a_Context">The context to bind to </param>
+	void bind(ID3D11DeviceContext* a_Context);
+private:	
+	/// <summary>
+	/// Converts a given assimp vector to those used by the xnamath library
+	/// </summary>
+	/// <param name="a_Vector">The vector to convert </param>
+	/// <returns>The converted-to 2D vector </returns>
+	XMFLOAT2 toXMFloat2(aiVector3D a_Vector);
+	
+	/// <summary>
+	/// Converts a given assimp vector to those used by the xnamath library
+	/// </summary>
+	/// <param name="a_Vector">The vector to convert </param>
+	/// <returns> The converted-to 3D vector</returns>
+	XMFLOAT3 toXMFloat3(aiVector3D a_Vector);
+	
+	/// <summary>
+	/// Converts a given assimp color to those used by the xnamath library
+	/// </summary>
+	/// <param name="a_Color"> The color vector to convert </param>
+	/// <returns> The converted-to 4D vector, representing a color </returns>
+	XMFLOAT4 toXMFloat4(aiColor4D a_Color);
+	
+	/// <summary>
+	/// Retrieves the mesh data at the given filepath, so it can be used for storing on the GPU
+	/// </summary>
+	/// <param name="a_Filepath">The path to the mesh </param>
+	void loadMeshdata(const std::string& a_Filepath);
+	
+	/// <summary>
+	/// Builds the index buffer from the mesh data
+	/// </summary>
+	/// <param name="a_Device">The Direct3D device to build the index buffer for </param>
+	void buildIndexBuffer(ID3D11Device* a_Device);
+	
+	/// <summary>
+	/// Builds the index buffer from the mesh data
+	/// </summary>
+	/// <param name="a_Device">The Direct3D device to build the vertex buffer for </param>
+	void buildVertexBuffer(ID3D11Device* a_Device);
 };
