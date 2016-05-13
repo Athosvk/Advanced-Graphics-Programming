36933694dfebe669de8ae9178999a04440b7f9a2
Athos <athosvankralingen@hotmail.com>
2016-05-13 11:49:56 +0200

    Added some more documentation

--- Assignment 3/Assignment3.h
+++ Assignment 3/Assignment3.h
@@ -15,10 +15,21 @@ private:
     float mRadius;
 
     POINT mLastMousePos;
-
-    Triangle m_BlueTriangle = Triangle(1.0f, XMVectorSet(0.0f, 0.0f, 1.0f, 1.0f));
-    Triangle m_YellowTriangle = Triangle(1.0f, XMVectorSet(1.0f, 1.0f, 0.0f, 0.5f));
-    Triangle m_OccludingTriangle = Triangle(1.0f, XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f));
+	
+	/// <summary>
+	/// The blue triangel rendered on the screen
+	/// </summary>
+	Triangle m_BlueTriangle = Triangle(1.0f, XMVectorSet(0.0f, 0.0f, 1.0f, 1.0f));
+	
+	/// <summary>
+	/// The partially transparent yellow triangle
+	/// </summary>
+	Triangle m_YellowTriangle = Triangle(1.0f, XMVectorSet(1.0f, 1.0f, 0.0f, 0.5f));
+	
+	/// <summary>
+	/// The triangle occluding the other two triangles partially
+	/// </summary>
+	Triangle m_OccludingTriangle = Triangle(1.0f, XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f));
 
 public:
     Assignment3(HINSTANCE hInstance);
@@ -33,7 +44,21 @@ public:
     void OnMouseUp(WPARAM btnState, int x, int y);
     void OnMouseMove(WPARAM btnState, int x, int y);
 
-private:
-    ID3DX11Effect* InitialiseShader(const std::wstring& a_FilePath);
-    void BuildVertexLayout(ID3DX11Effect* a_ReferenceShader);
+private:	
+	/// <summary>
+	/// Initialises the shader at the given file path
+	/// </summary>
+	/// <param name="a_FilePath">The path to the shader </param>
+	/// <returns> The resource handle to the shader </returns>
+	ID3DX11Effect* InitialiseShader(const std::wstring& a_FilePath);
+	
+	/// <summary>
+	/// Builds the vertex layout using the given reference shader
+	/// </summary>
+	/// <remarks> 
+	/// Reference shader is necessary since Direct3D does not allow building
+	/// an input layout without one
+	/// </remarks>
+	/// <param name="a_ReferenceShader">The a_ reference shader.</param>
+	void BuildVertexLayout(ID3DX11Effect* a_ReferenceShader);
 };
